cea5cdbd78be8230501b9239ae604c4fd81769eb
Carlos Queiroz <cqueiroz@gopivotal.com>
2016-03-28 10:55:11 +0800

    add end of line char for row mapper.

--- src/main/java/io/pivotal/spring/xd/jdbcgpfdist/jdbc/CustomRowMapper.java
+++ src/main/java/io/pivotal/spring/xd/jdbcgpfdist/jdbc/CustomRowMapper.java
@@ -2,6 +2,7 @@ package io.pivotal.spring.xd.jdbcgpfdist.jdbc;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
@@ -15,20 +16,30 @@ public class CustomRowMapper implements RowMapper<String> {
 
     private final Log log = LogFactory.getLog(CustomRowMapper.class);
 
+    private String columnDelimiter;
+
 
     @Override
     public String mapRow(ResultSet resultSet, int i) throws SQLException {
         StringBuilder sb = new StringBuilder();
         int columnsCount = resultSet.getMetaData().getColumnCount();
 
-        for(int j = 1; j <= columnsCount;j++){
+        for(int j = 1; j < columnsCount;j++){
             sb.append(resultSet.getObject(j));
-            sb.append("\t");
-
+            sb.append(columnDelimiter);
         }
 
-        String row = sb.toString();
+        String row = sb.append(resultSet.getObject(columnsCount)).append("\n").toString();
         log.info(row);
         return row;
+    }
+
+    public String getColumnDelimiter() {
+        return columnDelimiter;
+    }
+
+    @Autowired
+    public void setColumnDelimiter(String columnDelimiter) {
+        this.columnDelimiter = columnDelimiter;
     }
 }
